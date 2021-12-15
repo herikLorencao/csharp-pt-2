7af07afea5495f2815c7a83ec5a9e9144d5939c9
Herik S. Lorenção <herik.lorencao@itixti.com.br>
2021-12-15 10:38:08 -0300

    adicionando encapsulamento

---
+++ 06-ByteBank/ContaCorrente.cs
@@ -0,0 +1,47 @@
+﻿namespace _06_ByteBank
+{
+    public class ContaCorrente
+    {
+        private double _saldo = 100;
+
+        public Cliente Titular { get; set; }
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+
+        public double Saldo
+        {
+            get { return _saldo; }
+            set
+            {
+                if (_saldo < 0)
+                    return;
+
+                _saldo = value;
+            }
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (_saldo < valor)
+                return false;
+
+            _saldo -= valor;
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (_saldo < valor)
+                return false;
+
+            _saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+    }
+}
---
+++ 06-ByteBank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _06_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente = new Cliente();
+            cliente.Nome = "Ana";
+            cliente.CPF = "123.325.256-12";
+            cliente.Profissao = "Desenvolvedor C#";
+
+            ContaCorrente contaCorrente = new ContaCorrente();
+            contaCorrente.Titular = cliente;
+
+            Console.WriteLine(contaCorrente.Titular.Nome);
+            Console.ReadLine();
+        }
+    }
+}
